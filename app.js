8bd69048720f18bc1a3aec71cd15af0ede9ab73f
shreyaAwasthiNashtech <shreya.awasthi@nashtechglobal.com>
2025-09-24 10:24:17 +0530

    Adding the gitleaks-before.json report with a leak found

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 // const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE";  
 
 app.get('/', (req, res) => {
-  res.send(`Hello DevSecOps! API key (env): ${process.env.MY_API_KEY || 'not set'}`);
+  res.send(`Hello DevSecOps! API key (env): ${!!process.env.MY_API_KEY}`);
 });
 
 app.listen(port, () => console.log(`App running on http://localhost:${port}`));
