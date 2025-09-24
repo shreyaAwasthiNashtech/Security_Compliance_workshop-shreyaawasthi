567666e010390ec8e4e7e89f2ff541d880061a04
shreyaAwasthiNashtech <shreya.awasthi@nashtechglobal.com>
2025-09-24 10:38:58 +0530

    Uploading the gitleak result after removing the secret code which was hardcoaded (leak found)

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE";  
-
 app.get('/', (req, res) => {
   res.send(`Hello DevSecOps! API key (env): ${!!process.env.MY_API_KEY}`);
 });
